d92dd927aa15a3c702347b325f10ba5a0a7d6e95
Daniel Pierri <cyberpierri@gmail.com>
2023-03-28 21:06:34 -0300

    Updates

---
+++ precache-manifest.9bc83d1248d15f26d5c10fac5873a18d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b673dadb9abac6d059a",
+    "url": "/bigcontrol/static/css/main.a1b48103.chunk.css"
+  },
+  {
+    "revision": "6b673dadb9abac6d059a",
+    "url": "/bigcontrol/static/js/main.6b673dad.chunk.js"
+  },
+  {
+    "revision": "035c8df7a81b1543ee77",
+    "url": "/bigcontrol/static/js/1.035c8df7.chunk.js"
+  },
+  {
+    "revision": "417159bd5e64ee61664c",
+    "url": "/bigcontrol/static/js/runtime~main.417159bd.js"
+  },
+  {
+    "revision": "be7ced7713bcc56eef4f26c355b739b5",
+    "url": "/bigcontrol/static/media/hero-background.be7ced77.png"
+  },
+  {
+    "revision": "5c98b6f5226ba7b37ab773301039987d",
+    "url": "/bigcontrol/static/media/big-control-logo.5c98b6f5.png"
+  },
+  {
+    "revision": "dda567363ed474da03cd3ae59cbe9fe3",
+    "url": "/bigcontrol/index.html"
+  }
+];
