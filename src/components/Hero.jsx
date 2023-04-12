4e44ad99a42748001d3c99b3d2743b5e4bfca3d3
Daniel Pierri <cyberpierri@gmail.com>
2023-04-12 17:40:13 -0300

    hero changes and random questions function

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,64 @@
-import React from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import Navbar from "./Navbar"
 import "../App.scss"
 import wpplogo from "../img/whatsapp-128.png"
 
+const questions = [
+  "Lorem lorem lorem lorem?",
+  "Lorem2 lorem2 lorem2 lorem2?",
+  "Lorem3 lorem3 lorem3 lorem3?"
+]
+
 export default function Hero() {
+  const [newQuestion, setNewQuestion] = useState(questions[0])
+
+  const shuffle = useCallback(() => {
+    const index = Math.floor(Math.random() * questions.length)
+    setNewQuestion(questions[index])
+  }, [])
+
+  useEffect(() => {
+    const intervalID = setInterval(shuffle, 5000)
+    return () => clearInterval(intervalID)
+  }, [shuffle])
+
   return (
     <>
       <div className="hero__bg">
         <div className="hero">
+
           <Navbar />
+
           <div className="hero__main">
+
             <div className="hero__content">
               <h1 className="hero__title">BIG CONTROL</h1>
               <p className="hero__description">
-                Segurança Perimetral,
+                Segurança Eletrônica,
                 <br />
-                CFTV e Controle de Acesso
+                Perimetral e Controle de Acesso
               </p>
             </div>
-            <div className="hero-cta__container">
-              <p className="hero-cta__text">Confira nossos serviços</p>
-              <a href="index.html" className="hero-cta">Conhecer</a>
+
+            <div className="hero__cta__wrapper">
+              <div className="hero__cta__container">
+                <p className="hero__cta__title">
+                  Projetos, Execução e Manutenção
+                </p>
+                <p className="hero__cta__text">Confira nossos serviços</p>
+                <a href="index.html" className="hero__cta">Conhecer</a>
+              </div>
+              <div className="hero__question__container">
+                <p className="hero__question">{newQuestion}</p>
+              </div>
             </div>
 
-            <div className="hero-contact__container">
-              <img src={wpplogo} alt="WhatsApp logo" />
-              <p>(21)99999-9999</p>
-            </div>
+          </div>
+
+
+          <div className="hero__contact__container">
+            <img src={wpplogo} alt="WhatsApp logo" />
+            <p>(21)99999-9999</p>
           </div>
         </div>
       </div>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,6 +1,9 @@
 import React from "react"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+import cftvServicos from "../img/cftv-servicos.jpg"
+import cercaEletricaServicos from "../img/cerca-eletrica-servicos.jpg"
+import controleDeAcessoServicos from "../img/controle-de-acesso-servicos.jpg"
 
 export default function Services() {
   return (
@@ -9,15 +12,23 @@ export default function Services() {
       <div className="services__container">
         <main className="services">
           <div className="service service-1">
-            
+            <div className="service__img__container">
+              <img src={cftvServicos} className="service__img" alt="câmera"/>
+            </div>
             <h2 className="services__title">CFTV</h2>
             <p className="services__text">A função principal de segurança perimetral é inibir a ação de possíveis invasores. Para isso as principal ferramentas utilizadas são a concertina e a cerca elétrica. Outras opções são os sensores de barreira ou câmeras perimetrais para identificar se a área do perímetro foi invadida. Em geral, quando percebem o sistema de segurança, possíveis infratores avaliam a situação e desistem.</p>
           </div>
           <div className="service service-2">
+            <div className="service__img__container">
+              <img src={cercaEletricaServicos} className="service__img" alt="câmera"/>
+            </div>
             <h2 className="services__title">Segurança Perimetral</h2>
             <p className="services__text">A função principal de segurança perimetral é inibir a ação de possíveis invasores. Para isso as principal ferramentas utilizadas são a concertina e a cerca elétrica. Outras opções são os sensores de barreira ou câmeras perimetrais para identificar se a área do perímetro foi invadida. Em geral, quando percebem o sistema de segurança, possíveis infratores avaliam a situação e desistem.</p>
           </div>
           <div className="service service-3">
+            <div className="service__img__container">
+              <img src={controleDeAcessoServicos} className="service__img" alt="câmera"/>
+            </div>
             <h2 className="services__title">Controle de Acesso</h2>
             <p className="services__text">A função principal de segurança perimetral é inibir a ação de possíveis invasores. Para isso as principal ferramentas utilizadas são a concertina e a cerca elétrica. Outras opções são os sensores de barreira ou câmeras perimetrais para identificar se a área do perímetro foi invadida. Em geral, quando percebem o sistema de segurança, possíveis infratores avaliam a situação e desistem.</p>
           </div>
