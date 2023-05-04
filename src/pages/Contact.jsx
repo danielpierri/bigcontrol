0ed104ee848392bc5633f57b43f8b017b7511734
Daniel Pierri <cyberpierri@gmail.com>
2023-05-04 15:11:28 -0300

    working on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,29 @@
 import React from "react"
 import Navbar from "../components/Navbar"
-import Main from "../components/Main"
 import Footer from "../components/Footer"
 
 export default function Contact() {
   return (
     <div>
       <Navbar />
-      <h1>This is the about page</h1>
+      <div className="contact">
+        <div className="contact__form__container">
+          <form action="#">
+            <fieldset>
+              <legend>Nome</legend>
+              <label for="name"></label>
+              <input type="text" id="name" name="name"
+              placeholder="Digite seu nome"></input>
+            </fieldset>
+            <fieldset>
+              <legend>Email</legend>
+              <label for="email"></label>
+              <input type="email" id="email" name="email"
+              placeholder="Digite seu email"></input>
+            </fieldset>
+          </form>
+        </div>
+      </div>
       <Footer />
     </div>
   )
