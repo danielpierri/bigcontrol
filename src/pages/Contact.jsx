4ff9a05d7a02379532d549a205a28fc9098f5235
Daniel Pierri <cyberpierri@gmail.com>
2023-05-05 11:42:26 -0300

    starting contact form layout

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,22 +4,28 @@ import Footer from "../components/Footer"
 
 export default function Contact() {
   return (
-    <div>
+    <div className="contact__viewport">
       <Navbar />
       <div className="contact">
         <div className="contact__form__container">
-          <form action="#">
-            <fieldset>
+          <form className="form" action="#">
+            <fieldset className="fieldset input-name">
               <legend>Nome</legend>
               <label for="name"></label>
               <input type="text" id="name" name="name"
               placeholder="Digite seu nome"></input>
             </fieldset>
-            <fieldset>
+            <fieldset className="fieldset input-email">
               <legend>Email</legend>
               <label for="email"></label>
               <input type="email" id="email" name="email"
               placeholder="Digite seu email"></input>
+            </fieldset>
+            <fieldset className="fieldset input-message">
+              <legend>Mensagem</legend>
+              <label for="text"></label>
+              <textarea type="text" id="message" name="message"
+              placeholder="Escreva sua mensagem..."></textarea>
             </fieldset>
           </form>
         </div>
