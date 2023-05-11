fc04aff47bd5b93e83cd0d0f06f5acfd2dfce34f
Daniel Pierri <cyberpierri@gmail.com>
2023-05-11 18:36:36 -0300

    --

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react"
 // import Fade from "./hooks/Fade"
 import Navbar from "./Navbar"
+import Phone from "./Phone"
 import "../App.scss"
-import wpplogo from "../img/whatsapp-128.png"
 
 const FADE_INTERVAL_MS = 5000
 const WORD_CHANGE_INTERVAL_MS = FADE_INTERVAL_MS * 2
@@ -69,11 +69,7 @@ export default function Hero() {
 
           </div>
 
-          <div className="hero__contact__container">
-
-              <a href="https://wa.me/5521984870140?" target="_blank" rel="noreferrer"><img src={wpplogo} alt="WhatsApp logo" /></a>
-              <a href="tel:+5521984870140"className="hero__phone" >(21) 98487-0140</a>
-          </div>
+          <Phone />
           <div className="hero__hours">
             <p>Segunda a sexta, 9h às 18h</p>
           </div>
